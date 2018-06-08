c22ed540527aa4ac170fff22b96f17f30d6b0735
caleb <caleb.sears22@gmail.com>
2018-06-07 20:09:23 -0400

    updating api routing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,23 @@
-const path = require('path')
+var path = require('path')
+var bodyParser = require('body-parser')
+var tableData = require('../data/tableData')
+var waitingData = require('../data/waitingListData')
 
-exports.check_route = function(route, app){
-    switch(route){
-        case '/':
-            return app.get(path.join(__dirname, 'home.html'))
-    }
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+exports.check_route = function(app){
+    // Updates reservation array on api call
+    app.post('/api/reserve', (req, res)=>{
+        var new_reservation = req.body
+        console.log(new_reservation)
+    })
+    // Return array of reservation objects on api call
+    app.get('/api/tables', (req, res)=>{
+        res.send(tableData.reservations)
+    })
+    // Return array of waiting list objecs on api call
+    app.get('/api/waiting', (req, res)=>{
+        res.send(waitingData.waiting_list)
+    })
 }
